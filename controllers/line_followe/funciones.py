385c1a0e6a3995e52b0e94eccec8e444d30ad818
lfrecalde1 <lfrecalde1@espe.edu.ec>
2020-11-25 03:10:56 -0500

    PROYECTO EVASOR

--- controllers/line_followe/funciones.py
+++ controllers/line_followe/funciones.py
@@ -1,8 +1,15 @@
 import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib as mpl
 def conversion(v,r,L):
     T=np.matrix([[r/2,r/2],[r/L,-r/L]])
     tranformacion_ruedas=np.linalg.inv(T)@v
     return tranformacion_ruedas[0,0],tranformacion_ruedas[1,0]
+
+def conversion_1(W,r,L):
+    T=np.matrix([[r/2,r/2],[r/L,-r/L]])
+    generales=T@W
+    return generales[0,0],generales[1,0]
 
 def pid(sp,real,err_1,err_2,u_1,kp,ki,kd,ts):
     err=np.tanh(sp-real)
@@ -31,11 +38,13 @@ def conversion_sensor(sensor1,sensor2,sensor3):
     return dato
     
 def angulo_sensores(sensor1,sensor2,sensor3):
+    auxiliar=1
     if(sensor1==1 and sensor2==0 and sensor3==0):
         valor=-1
+        auxiliar=valor
         
     elif(sensor1==1 and sensor2==1 and sensor3==0):
-        valor=-0.5   
+        valor=-0.5
          
     elif(sensor1==0 and sensor2==1 and sensor3==0):
         valor=0
@@ -45,11 +54,88 @@ def angulo_sensores(sensor1,sensor2,sensor3):
          
     elif(sensor1==0 and sensor2==0 and sensor3==1):
         valor=1
+        auxiliar=valor
         
     elif(sensor1==1 and sensor2==1 and sensor3==1):
         valor=2
         
     else:
-        valor=3
+        valor=auxiliar
         
     return valor
+def tranformacion_cordenadas(x,y,z,phi,teta):
+    T1=np.matrix([[np.cos(phi),0,np.sin(phi)],[0,1,0],[-np.sin(phi),0,np.cos(phi)]])
+    T2=np.matrix([[np.cos(teta),-np.sin(teta),0],[np.sin(teta),np.cos(teta),0],[0,0,1]])
+    relativo=np.array([[x],[y],[z]])
+    real=T1@T2@relativo
+    return real[0,0],real[1,0],real[2,0]
+
+def grafica(sty,titulo,x,y,etiqueta,ejex,ejey,color):
+    mpl.style.use(sty)
+    fig, ax = plt.subplots()
+    ax.set_title(titulo.format(sty), color='0')
+    ax.set_xlabel(ejex)
+    ax.set_ylabel(ejey)
+    ax.plot(x, y, color, label=etiqueta)
+    ax.grid(linestyle='--', linewidth='0.3', color='black')
+    legend = ax.legend(loc='upper right', shadow=False, fontsize='small')
+    plt.show()
+
+def grafica_c(sty,titulo,x,y,etiqueta,ejex,ejey,color,x_1,y_1,etiqueta_1,color_1):
+    mpl.style.use(sty)
+    fig, ax = plt.subplots()
+    ax.set_title(titulo.format(sty), color='0')
+    ax.set_xlabel(ejex)
+    ax.set_ylabel(ejey)
+    ax.plot(x, y, color,label=etiqueta)
+    ax.plot(x_1,y_1,color_1,label=etiqueta_1)
+    ax.plot()
+    ax.grid(linestyle='--', linewidth='0.3', color='black')
+    legend = ax.legend(loc='upper right', shadow=False, fontsize='small')
+    plt.show()
+
+def obstaculos(distance,ground,err_1,err_2,w_1,kp,ki,kd,t_sample):
+    linea_real=conversion_sensor(ground[0].getValue(),ground[1].getValue(),ground[2].getValue())
+    pepito,err=pid(0,linea_real,err_1,err_2,w_1,kp,ki,kd,t_sample)
+    print(linea_real)
+    if distance[1].getValue() > 100.0:
+        w=1
+        u=0.01
+        
+    elif distance[0].getValue() > 100.0:
+        w=-1
+        u=u=0.01
+        
+    elif distance[3].getValue() > 100.0:
+        w=1
+        u=u=0.01
+       
+    elif distance[2].getValue() > 100.0:
+        w=-1
+        u=u=0.01
+        
+    else:
+        #w,err=pid(0,linea_real,err_1,err_2,w_1,kp,ki,kd,t_sample)
+        w=0
+        u=0.02
+    print(linea_real)
+    return u,w,err
+
+def Jacobiano(v,phi,a):
+    J=np.matrix([[np.cos(phi),-a*np.sin(phi)],[np.sin(phi),a*np.cos(phi)]])
+    hp=J*v
+    return hp
+
+def euler(z,zp,t_sample):
+    z=z+zp*t_sample
+    return z
+
+def vel(ruedas,lastPostL,lastPostR,t_sample):
+    newPostR = ruedas[0].getValue()
+    velPosR = (newPostR - lastPostR)/t_sample
+    lastPostR = newPostR
+
+    newPostL = ruedas[1].getValue()
+    velPosL = (newPostL - lastPostL)/t_sample
+    lastPostL = newPostL
+    return velPosR,velPosL,lastPostL,lastPostR
--- controllers/line_followe/line_followe.py
+++ controllers/line_followe/line_followe.py
@@ -18,16 +18,36 @@ for i in range(2):
     wheels.append(robot.getMotor(wheelsNames[i]))
     wheels[i].setPosition(float('inf'))
     wheels[i].setVelocity(0.0)
-   
+
 ground=[]
 ground_names=['gs0','gs1','gs2']
 for i in range(3):
     ground.append(DistanceSensor(ground_names[i]))
     ground[i].enable(timestep)
 
+distance=[]
+distance_names=['ps7','ps0','ps6','ps1']
+for i in range(4):
+    distance.append(DistanceSensor(distance_names[i]))
+    distance[i].enable(timestep)
+
+ruedas=[]
+ruedas_names=['right wheel sensor','left wheel sensor']
+for i in range(2):
+    ruedas.append(PositionSensor(ruedas_names[i]))
+    ruedas[i].enable(timestep)
+
+# DEFINCION DEL SENSOR GPS
+gps = GPS("gps")
+gps.enable(timestep)
+
+# DFINCION DE LA IMU
+imu = InertialUnit("inertial unit")
+imu.enable(timestep)
+
 # Definir el tiempo de sampleo del sistema
 t_sample=0.1
-t_final=30+t_sample
+t_final=35+t_sample
 t=np.arange(0,t_final,t_sample)
 t=t.reshape(1,t.shape[0])
 
@@ -37,15 +57,68 @@ L=0.074
 a=0.074/2
 
 #velocidades generales
-u=-0.0*np.ones((t.shape[0],t.shape[1]))
+u=0.0*np.ones((t.shape[0],t.shape[1]))
 w=0*np.ones((t.shape[0],t.shape[1]))
+
+u_r=0*np.ones((t.shape[0],t.shape[1]+1))
+ww_r=0*np.ones((t.shape[0],t.shape[1]+1))
 
 #velocidades de cada rueda
 w_r=0*np.ones((t.shape[0],t.shape[1]))
 w_l=0*np.ones((t.shape[0],t.shape[1]))
 
+w_r_r=0*np.ones((t.shape[0],t.shape[1]+1))
+w_l_r=0*np.ones((t.shape[0],t.shape[1]+1))
+
+#Definicion de los errores del pid
+err_1=0
+err_2=0
+w_1=0
+# deficinion de los vectores vacios del ssistema
+#Posiciones del robot
+x=np.zeros((t.shape[0],t.shape[1]+1))
+y=np.zeros((t.shape[0],t.shape[1]+1))
+phi=np.zeros((t.shape[0],t.shape[1]+1))
+
+x_r=np.zeros((t.shape[0],t.shape[1]+1))
+y_r=np.zeros((t.shape[0],t.shape[1]+1))
+phi_r=np.zeros((t.shape[0],t.shape[1]+1))
+
+if robot.step(timestep) != -1:
+
+    w_r_r[0,0]=wheels[0].getVelocity()
+    w_l_r[0,0]=wheels[1].getVelocity()
+    W=np.array([[w_r_r[0,0]],[w_l_r[0,0]]])
+    u_r[0,0],ww_r[0,0]=conversion_1(W,r,L)
+
+    #posiciones iniciales lectura del robot
+    posicion = gps.getValues()
+    #Tranformacion de las posiciones reales al sistema de referencia deseado
+    x_real,y_real,z_real=tranformacion_cordenadas(posicion[0],posicion[1],posicion[2],-np.pi/2,-np.pi/2)
+    
+    phi[0,0]=imu.getRollPitchYaw()[2]
+    phi_r[0,0]=imu.getRollPitchYaw()[2]
+
+    x[0,0]=x_real+a*np.cos(phi[0,0])
+    y[0,0]=y_real+a*np.sin(phi[0,0])
+
+    x_r[0,0]=x_real+a*np.cos(phi_r[0,0])
+    y_r[0,0]=y_real+a*np.sin(phi_r[0,0])
+
+lastPostL=0
+lastPostR=0
+
+kp=0.5
+ki=0
+kd=0
+
 for k in range(0,t.shape[1]):
     if robot.step(timestep) != -1:
+        
+        u[0,k],w[0,k],err=obstaculos(distance,ground,err_1,err_2,w_1,kp,ki,kd,t_sample)
+
+
+
         ## Vector de velocidades del sistema
         v=np.array([[u[0,k]],[w[0,k]]])
         
@@ -54,12 +127,48 @@ for k in range(0,t.shape[1]):
         
         wheels[0].setVelocity(w_r[0,k])
         wheels[1].setVelocity(w_l[0,k])
-        linea_real=conversion_sensor(ground[0].getValue(),ground[1].getValue(),ground[2].getValue())
-        
-        print(linea_real)    
+
+       
+        #Transformacion para los valores reales del gps
+        w_r_r[0,k+1],w_l_r[0,k+1],lastPostL,lastPostR=vel(ruedas,lastPostL,lastPostR,t_sample)
+        #w_r_r[0,k+1]=wheels[0].getVelocity()
+        #w_l_r[0,k+1]=wheels[1].getVelocity()
+
+        W=np.array([[w_r_r[0,k+1]],[w_l_r[0,k+1]]])
+        u_r[0,k+1],ww_r[0,k+1]=conversion_1(W,r,L) 
+
+        v_r=np.array([[u_r[0,k+1]],[ww_r[0,k+1]]])
+
+        ## Velocidad del punto modelado
+        hp=Jacobiano(v_r,phi[0,k],a)
+    
+        ## Obtener las velocidades en Cada eje
+        xp=hp[0,0]
+        yp=hp[1,0]
+
+        x_r[0,k+1]=euler(x_r[0,k],xp,t_sample)
+        y_r[0,k+1]=euler(y_r[0,k],yp,t_sample)
+        phi_r[0,k+1]=euler(phi[0,k],ww_r[0,k+1],t_sample)
+
+        posicion = gps.getValues()
+
+        x_real,y_real,z_real=tranformacion_cordenadas(posicion[0],posicion[1],posicion[2],-np.pi/2,-np.pi/2)
+        phi[0,k+1]=imu.getRollPitchYaw()[2]
+        x[0,k+1]=x_real+a*np.cos(phi[0,k+1])
+        y[0,k+1]=y_real+a*np.sin(phi[0,k+1])
+
+        print(x[0,k+1],y[0,k+1],phi[0,k+1])
+        # Actualizacion de los valores apra le pid
+        err_2=err_1
+        err_1=err
+        w_1=w[0,k]
+           
     
 wheels[0].setVelocity(0)
 wheels[1].setVelocity(0)
-
-
+grafica_c('default','Trayectorias',x[0,:],y[0,:],'$h$','$x[m]$','$y[m]$','g',x_r[0,:],y_r[0,:],'$h_r$','r--')
+grafica_c('default','Velocidad derecha',t[0,:],w_r[0,:],'$w_r$','$t[s]$','$rad/s$','g',t[0,:],w_r_r[0,0:t.shape[1]],'$w_{rr}$','r--')
+grafica_c('default','Velocidad Izquierda',t[0,:],w_l[0,:],'$w_r$','$t[s]$','$rad/s$','g',t[0,:],w_l_r[0,0:t.shape[1]],'$w_{rr}$','r--')
+grafica_c('default','Velocidad lineal',t[0,:],u[0,:],'$u$','$t[s]$','$m/s$','g',t[0,:],u_r[0,0:t.shape[1]],'$u_r$','r--')
+grafica_c('default','Velocidad angular',t[0,:],w[0,:],'$w$','$t[s]$','$rad/s$','g',t[0,:],ww_r[0,0:t.shape[1]],'$w_r$','r--')
 print("FINALIZACION DEL PROGRAMA")
